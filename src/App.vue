623ddfe49d00ce375681498f95e18164fd0f61c5
One290 <lenya.dorovskoy@mail.ru>
2025-01-27 20:18:55 +0800

    dbnio

--- src/App.vue
+++ src/App.vue
@@ -3,9 +3,7 @@
 </script>
 
 <template>
-  <div class="container border border-2 rounded second-border-color p-0 w-100">
     <h1>Hello from Vue 3 + Bootstrap 5</h1>
-  </div>
 </template>
 
 <style scoped>
